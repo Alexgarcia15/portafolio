f5a917b148c288ab3a244ee8186ad3774e16c1d8
Hector Garcia <hectrramon001@gmail.com>
2026-05-06 10:37:19 -0400

    Organizo proyecto en carpetas css y js

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log("Portafolio cargado 🚀");
-
-// efecto simple
-document.querySelectorAll(".card").forEach(card => {
-  card.addEventListener("click", () => {
-    alert("Proyecto en construcción 🔧");
-  });
-});
