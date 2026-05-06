6d9557d09088b7144118d9287eb6a6f7b79d667f
Hector Garcia <hectrramon001@gmail.com>
2026-05-06 09:30:41 -0400

    Carga inicial de mi portafolio

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Portafolio cargado 🚀");
+
+// efecto simple
+document.querySelectorAll(".card").forEach(card => {
+  card.addEventListener("click", () => {
+    alert("Proyecto en construcción 🔧");
+  });
+});
